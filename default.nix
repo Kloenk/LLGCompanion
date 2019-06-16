42301fa7091e8d2a0e8c5ae831e9abc198c54be0
Kloenk <kloenk@kloenk.de>
2019-06-16 09:53:24 +0200

    rewrite defaul.nix

--- default.nix
+++ default.nix
@@ -1,30 +1,25 @@
 # This file defines a function that takes a single optional argument 'pkgs'
 # If pkgs is not set, it defaults to importing the nixpkgs found in NIX_PATH
 {
-    pkgs ? import <nixpkgs> {},
+    pkgs, 
     nodejs-11_x,
     yarn,
     gitMinimal,
     lib
 }:
-let
-   # Convenience alias for the standard environment
-   stdenv = pkgs.stdenv;
-in rec {
-  # Defines our application package
-  app = stdenv.mkDerivation {
-    name = "llgCompanion";
-    # The source code is stored in our 'app' directory
-    src = ./.;
-    # Our package depends on the nodejs package defined above
-    buildInputs = [ nodejs-11_x yarn gitMinimal ];
 
-    makeFlags = [ "PREFIX=$(out)" ];
-    installTargets = "install";
-  };
-  meta = with lib; {
-    description = "Companion for planinfo and dsb mix as seen at llg";
+lib.stdenv.mkDerivation rec {
+  name = "shelfie-${version}";
+  version = "1.2.1";
+
+  srv = ./.;
+
+  buildInputs = [ nodejs-11_x yarn gitMinimal ];
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  met = with lib; {
+    description = "Companion fo planInfo and dsb mix";
     license = licenses.agpl3;
     platforms = platforms.all;
   };
-}
+};
